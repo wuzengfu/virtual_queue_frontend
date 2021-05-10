6b37f0adced5db62bb0ac9f7db75630d30431e9d
wuzengfu <wuzengfu666@gmail.com>
2021-05-10 13:11:56 +0800

    functions for create and draw waiting-time panels added

--- scripts/modules/google-charts.mjs
+++ scripts/modules/google-charts.mjs
@@ -8,6 +8,59 @@ export function createAreaChart(chart) {
     chart.googleChart = new google.visualization.AreaChart(chartDom);
 }
 
+/**
+ * Create HTML DOM elements for the four waiting-time panels
+ * <ul>
+ *     div.panels >
+ *     <li>&nbsp p.key</li>
+ *     <li>&nbsp p.value</li>
+ * </ul>
+ *
+ * @param {Object} chart - the properties of the waiting-time panel
+ */
+export function createWaitingTimePanels(chart) {
+    const {
+        chartDom,
+        panels
+    } = chart;
+
+    let panelContainer, key, value;
+    for (let i = 0, length = panels.length; i < length; i++) {
+        //create DOM element
+        panelContainer = document.createElement('div');
+        key = document.createElement('p');
+        value = document.createElement('p');
+
+        //set class name for DOM element
+        panelContainer.setAttribute('class', 'panels');
+        key.setAttribute('class', 'key');
+        value.setAttribute('class', 'value');
+
+        //insert DOM elements to chart-container of waiting-time panels
+        panelContainer.appendChild(key);
+        panelContainer.appendChild(value);
+        chartDom.appendChild(panelContainer);
+    }
+}
+
+/**
+ * Insert value/data into the four panels
+ *
+ * @param {Object} data - the values for four panels {mean, median, max, min}
+ * @param {Object} chart - the properties of the waiting-time panel
+ */
+export function drawWaitingTimePanels(data, chart) {
+    const { panels } = chart;
+    let dom_key = document.getElementsByClassName('key');
+    let dom_value = document.getElementsByClassName('value');
+
+    //insert values into the four panels
+    for (let i = 0, length = Object.keys(data).length; i < length; i++) {
+        dom_key[i].innerHTML = panels[i].value;
+        dom_value[i].innerHTML = data[panels[i].key].toFixed(1);
+    }
+}
+
 export function drawGoogleChart(array, { googleChart, options }) {
     const data = google.visualization.arrayToDataTable(array);
     googleChart.draw(data, options);
