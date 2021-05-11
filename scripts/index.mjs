177a1001cfabda40fa871618f34bc7735c893dea
wuzengfu <wuzengfu666@gmail.com>
2021-05-11 12:48:26 +0800

    enable chart configuration when initialized

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,8 +1,9 @@
 import { initialize } from './modules/charts.mjs';
 import enableAutoRefresh from './modules/auto-refresh.mjs';
+import { enableChartConfig } from './modules/enableConfig.mjs';
 
 function makeCharts() {
-    initialize([enableAutoRefresh]);
+    initialize([enableAutoRefresh, enableChartConfig]);
 }
 
 google.charts.setOnLoadCallback(makeCharts);
